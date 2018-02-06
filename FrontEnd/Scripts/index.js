ea121f33aefdb4db43c589aa31e5549876e753ae
Luis Lopez <delucholuis@gmail.com>
2018-02-06 14:21:20 -0800

    Merge branch 'FEF-Exercise-5'
    Use the typeof keyword to print the data type of the hoursInDay constant to the console
    Use the typeof keyword to print the data type of the GOTCharacter variable to the console
    Declare a variable using let called GOTHouse but do not assign it a value
    Write an if statement that checks if GOTHouse is undefined
    If GOTHouse is undefined write to the console window a warning that the variable is undefined
    If GOTHouse is defined then write the value to a new div at the bottom of the houses.html page

--- FrontEnd/Scripts/index.js
+++ FrontEnd/Scripts/index.js
@@ -24,6 +24,7 @@ alert("Keeping up with the Targaryens, uh?");
 checkMyFavChar(myFavGOTChar);
 
 const hoursInDay= 24;
+
 var GOTcharacter = {
     name: "Jaime Lannister",
     house: "Lannister",
@@ -31,3 +32,16 @@ var GOTcharacter = {
 }
 console.log(hoursInDay);
 console.log(GOTcharacter);
+console.log(typeof hoursInDay);
+console.log(typeof GOTcharacter);
+let GOThouse;
+if (GOThouse === "undefined");
+console.log("undefined");
+if (GOThouse === "defined");
+// document.createElement("div");
+// document.footer.appendchild(div);
+
+var footer = document.createElement('div');
+footer.innerHTML = "GOThouse is defined!";
+//js part3
+
