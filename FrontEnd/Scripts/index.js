86440e63141b1bff1f845f5e2eea682e17aaff60
Luis Lopez <delucholuis@gmail.com>
2018-02-06 11:23:29 -0800

    Fixed Exercise-9
    checkMyFavChar(myFavGOTChar);

--- FrontEnd/Scripts/index.js
+++ FrontEnd/Scripts/index.js
@@ -4,20 +4,21 @@ function checkMyFavChar(character){
 
 if (character === "Tyrion Lannister")
 {
-return("A Lannister always pays its debts");
+alert("A Lannister always pays its debts");
 }
 
 else if (character === "Jon Snow")
 {
-return("Oh boy you are in for a big surprise!");
+alert("Oh boy you are in for a big surprise!");
 }
 
 else (character === "Jon Targaryen")
 {
-return("Keeping up with the Targaryens, uh?");
+alert("Keeping up with the Targaryens, uh?");
 }
 }
-var inpuAnswer = prompt("Enter GOT Character");
-var parseInt = parseInt(inputAnswer);
-var finalAnswer = calculateAnswer(parseInt);
-alert(finalAnswer);
+// var inpuAnswer = prompt("Enter GOT Character");
+// var parseInt = parseInt(inputAnswer);
+// var finalAnswer = calculateAnswer(parseInt);
+// alert(finalAnswer);
+checkMyFavChar(myFavGOTChar);
